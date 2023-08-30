d6b2c2e32ff7b127767ec49d4b16aa446440eef6
Michael Dahlberg <dahlberg@recursoft.org>
2023-08-30 12:17:37 -0400

    First commit of maxtemp.py .. doesnt work

---
+++ maxtemp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import requests
+
+lat = 40.99460211262978
+long = -77.56459180420583
+
+api_key = open('/home/dahlberg/PyBites/maxtemp/.tomorrow-io.api-key').read()
+
+print(api_key)
+
+url = "https://api.tomorrow.io/v4/weather/forecast?location=lat,long&apikey=api_key"
+
+print(url)
+
+headers = {"accept": "application/json"}
+
+response = requests.get(url, headers=headers)
+
+print(response.text)
