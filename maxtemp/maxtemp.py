7bdc996ab28aceb2c70c07d03940be6d9b044fe2
Michael Dahlberg <dahlberg@recursoft.org>
2023-08-31 08:57:01 -0400

    Fixed the auth not correct by stripping the newline character from the api-key in the file.  Must be a better way

--- maxtemp/maxtemp.py
+++ maxtemp/maxtemp.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 
 import requests
+import json
 
 lat = 40.99460211262978
-long = -77.56459180420583
+lon = -77.56459180420583
 
 api_key = open('/home/dahlberg/PyBites/maxtemp/.tomorrow-io.api-key').read()
-
-print(api_key)
+api_key = api_key.rstrip()
 
 url = f"https://api.tomorrow.io/v4/weather/forecast?\
-location={lat},{long}&apikey={api_key}"
+location={lat},{lon}&apikey={api_key}"
 
-print(url)
 
 headers = {"accept": "application/json"}
 
 response = requests.get(url, headers=headers)
 
-print(response.text)
+data = json.loads(response.text)
+
+print(data)
